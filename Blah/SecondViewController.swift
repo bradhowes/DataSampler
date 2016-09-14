22b9deef99e0c8f68f9d7ac1bbe01af25f727ef7
Brad Howes <bhowes@microsoft.com>
2016-09-14 23:01:37 +0200

    Initial Commit

---
+++ Blah/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Blah
+//
+//  Created by Brad Howes on 9/14/16.
+//  Copyright © 2016 Brad Howes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
